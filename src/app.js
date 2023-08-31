02438b31a90c13b3de4453397468173db9c7d325
usha <usha.chaudhary@esewa.com.np>
2023-08-31 17:25:32 +0545

    mongoose db connection, packaged added and route updated for all

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { router as categoryRouter } from "./routes/category.route.js";
-import { router as productRoter } from "./routes/product.route.js";
+import { router as productRouter } from "./routes/product.route.js";
+import { router as userRouter } from "./routes/user.route.js";
 export const app = express();
 
-app.use("/products", productRoter);
+app.use("/products", productRouter);
 app.use("/category", categoryRouter);
+app.use("/users", userRouter);
 // mainRoute.use("/user");
--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 import { app as mainApp } from "./app.js";
+import { errorHandler } from "./middleware/error.handler.js";
+//mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+const MONGO_URL = "mongodb://127.0.0.1:27017/ecommerce";
+mongoose.connect(MONGO_URL).then(
+  () => {
+    console.log("Server connection established!");
+  },
+  (error) => {
+    console.log(`An error occured while connecting server`);
+  }
+);
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
 // request body parser middlerware
 app.use(express.json());
 
 //main route entry point
-app.use("/api", mainApp);
 
+app.use("/api", mainApp);
+app.use(errorHandler);
 // app listing point
 app.listen(PORT, () => {
   console.log(`Server is running in the port: ${PORT}`);
